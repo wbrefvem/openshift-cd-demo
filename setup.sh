4e37fd1a5fcdf09f873579f554f360b94628cb7b
Will Refvem <wrefvem@redhat.com>
2018-01-19 11:57:47 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 oc new-project dev --display-name="Tasks - Dev"
 oc new-project stage --display-name="Tasks - Stage"
 oc new-project cicd --display-name="CI/CD"
-oc process -f cicd-template.yaml | oc create -f -
+oc process -f cicd-template-with-sonar.yaml | oc create -f -
